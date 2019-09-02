b8ea3ee8447e73aa8f452d620d146f68dff7f4c3
Andrew James <ajames20@gmail.com>
2019-09-01 19:29:59 -0500

    Add anagram solution

--- challenges/anagrams/index.js
+++ challenges/anagrams/index.js
@@ -1,6 +1,23 @@
 // given two strings write a function to determine the second string
 // is and anagram of the first string.
 
-function isAnagram(str1, str2) {}
+function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false
+
+  let charMap1 = {}
+  let charMap2 = {}
+  for (let char of str1) {
+    charMap1[char] = (charMap1[char] || 0) + 1
+  }
+  for (let char of str2) {
+    charMap2[char] = (charMap2[char] || 0) + 1
+  }
+  for (let key in charMap1) {
+    if (charMap1[key] !== charMap2[key]) {
+      return false
+    }
+  }
+  return true
+}
 
 module.exports = isAnagram
--- challenges/anagrams/index.test.js
+++ challenges/anagrams/index.test.js
@@ -2,4 +2,9 @@ const isAnagram = require('./')
 
 test('should return if return true if str2 is an anagram of str1', () => {
   expect(isAnagram('', '')).toBe(true)
+  expect(isAnagram('aaa', 'aaa')).toBe(true)
+  expect(isAnagram('anagram', 'ganaram')).toBe(true)
+  expect(isAnagram('awesome', 'awsome')).toBe(false)
+  expect(isAnagram('qwerty', 'ytrewq')).toBe(true)
+  expect(isAnagram('cat', 'car')).toBe(false)
 })
