457bbd3ecf2936d6e62ff32914c5600988a6d627
Andrew James <ajames20@gmail.com>
2019-09-02 19:02:12 -0500

    Add parameters

--- challenges/maximum-sum/index.js
+++ challenges/maximum-sum/index.js
@@ -1,7 +1,8 @@
 // Given an array consisting of n integers, find the contiguous subarray of given
 // length k that has the maximum value. And you need to output the maximum average value.
 
-function maximumSumBruteForce(arr) {}
-function maximumSum(arr) {}
+function maximumSumBruteForce(arr, n) {}
+
+function maximumSum(arr, n) {}
 
 module.exports = { maximumSum, maximumSumBruteForce }
