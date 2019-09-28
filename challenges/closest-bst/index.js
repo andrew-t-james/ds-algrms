9c48ce4b7cf42284860df905165b8689ac2c2372
Andrew James <ajames20@gmail.com>
2019-09-28 12:54:28 -0500

    Add second solution for find closest

--- challenges/closest-bst/index.js
+++ challenges/closest-bst/index.js
@@ -25,7 +25,25 @@ function findClosestValueInBst(tree, target) {
     }
   })
 
-  return Number(closest) // convert key from string to Num
+  return Number(closest) // convert key from string to int
+}
+
+function findClosestValueInBstTwo(tree, target, closest = Infinity) {
+  if (tree === null) return closest
+
+  if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+    closest = tree.value
+  }
+
+  if (target < tree.value) {
+    return findClosestValueInBstTwo(tree.left, target, closest)
+  } else if (target > tree.value) {
+    return findClosestValueInBstTwo(tree.right, target, closest)
+  } else {
+    return closest
+  }
+}
+
 }
 
 function findClosestValueInBstTwo(tree, target) {
