cb208ad657f255f0515c293117b8091179e5067c
Andrew James <ajames20@gmail.com>
2019-09-28 12:54:43 -0500

    Add tests for third solution

--- challenges/closest-bst/index.test.js
+++ challenges/closest-bst/index.test.js
@@ -1,5 +1,9 @@
 const bst = require('./Bst')
-const { findClosestValueInBst, findClosestValueInBstTwo } = require('.')
+const {
+  findClosestValueInBst,
+  findClosestValueInBstTwo,
+  findClosestValueInBstThree
+} = require('.')
 
 describe('Naive Solution', () => {
   test('Test Case #1', () => {
@@ -78,6 +82,48 @@ describe('More Better Solution', () => {
 
   test('Test Case #9', () => {
     expect(findClosestValueInBstTwo(bst, -1)).toBe(1)
+  })
+
+  test('Test Case #10', () => {
+    expect(findClosestValueInBstTwo(bst, 29751)).toBe(55000)
+  })
+})
+
+describe('WHILE loop solution', () => {
+  test('Test Case #1', () => {
+    expect(findClosestValueInBstThree(bst, 100)).toBe(100)
+  })
+
+  test('Test Case #2', () => {
+    expect(findClosestValueInBstThree(bst, 208)).toBe(208)
+  })
+
+  test('Test Case #3', () => {
+    expect(findClosestValueInBstThree(bst, 4500)).toBe(4500)
+  })
+
+  test('Test Case #4', () => {
+    expect(findClosestValueInBstThree(bst, 4501)).toBe(4500)
+  })
+
+  test('Test Case #5', () => {
+    expect(findClosestValueInBstThree(bst, -70)).toBe(-51)
+  })
+
+  test('Test Case #6', () => {
+    expect(findClosestValueInBstThree(bst, 2000)).toBe(1001)
+  })
+
+  test('Test Case #7', () => {
+    expect(findClosestValueInBstThree(bst, 6)).toBe(5)
+  })
+
+  test('Test Case #8', () => {
+    expect(findClosestValueInBstThree(bst, 30000)).toBe(55000)
+  })
+
+  test('Test Case #9', () => {
+    expect(findClosestValueInBstThree(bst, -1)).toBe(1)
   })
 
   test('Test Case #10', () => {
