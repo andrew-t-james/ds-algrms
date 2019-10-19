f6ecd60f90d44147287bda6c82cbe0b49518703c
Andrew James <ajames20@gmail.com>
2019-10-19 18:39:29 -0500

    Add longest subString

--- challenges/length-of-longest-substring/index.js
+++ challenges/length-of-longest-substring/index.js
@@ -1,3 +1,17 @@
-function longestSubstring(s) {}
+function longestSubstring(s) {
+  if (!s.length) return null
+  let start = 0
+  let longest = 0
+  let seen = {}
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i]
+    if (seen[char]) {
+      start = Math.max(start, seen[char])
+    }
+    longest = Math.max(longest, i - start + 1)
+    seen[char] = i + 1
+  }
+  return longest
+}
 
 module.exports = { longestSubstring }
