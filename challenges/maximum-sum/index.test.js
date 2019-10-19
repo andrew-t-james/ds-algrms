cfc90113872a085eb3eb1bc4f2e196b952d0254b
Andrew James <ajames20@gmail.com>
2019-10-19 18:49:50 -0500

    Update missed test in max sum

--- challenges/maximum-sum/index.test.js
+++ challenges/maximum-sum/index.test.js
@@ -8,7 +8,7 @@ test('should return the maximum value of n integers summed in an array', () => {
 })
 
 test('should return the maximum value of n integers summed in an array more better', () => {
-  expect(maximumSumBruteForce([], 3)).toBe(null)
+  expect(maximumSum([], 3)).toBe(null)
   expect(maximumSum([1, 2, 4, 5, 1, 5, 7], 3)).toBe(13)
   expect(maximumSum([2, 3, 5, 1, 3, 2], 3)).toBe(10)
   expect(maximumSum([10, 1, 33, 3, 12, 2], 2)).toBe(36)
