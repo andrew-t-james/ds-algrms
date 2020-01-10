d44c58e1d882a1c2d16ab335e1d756f1fd568933
andrew-t-james <ajames20@gmail.com>
2020-01-10 16:12:09 -0600

    Add second test

--- challenges/merge-sorted-list/index.test.js
+++ challenges/merge-sorted-list/index.test.js
@@ -1,37 +1,78 @@
 const { mergeSortedListsIterative } = require('.')
 
-const listOne = {
-  val: 1,
-  next: {
-    val: 2,
-    next: {
-      val: 4,
-      next: null
-    }
-  }
-}
-
-const listTwo = {
-  val: 1,
-  next: {
-    val: 3,
-    next: {
-      val: 4,
-      next: null
-    }
-  }
-}
-
-const expected = {
-  val: 1,
-  next: {
+test('should merge two sorted link lists', () => {
+  const listOne = {
     val: 1,
     next: {
       val: 2,
       next: {
-        val: 3,
+        val: 4,
+        next: null
+      }
+    }
+  }
+
+  const listTwo = {
+    val: 1,
+    next: {
+      val: 3,
+      next: {
+        val: 4,
+        next: null
+      }
+    }
+  }
+
+  const expected = {
+    val: 1,
+    next: {
+      val: 1,
+      next: {
+        val: 2,
         next: {
-          val: 4,
+          val: 3,
+          next: {
+            val: 4,
+            next: {
+              val: 4,
+              next: null
+            }
+          }
+        }
+      }
+    }
+  }
+  expect(mergeSortedListsIterative(listOne, listTwo)).toEqual(expected)
+})
+
+test('should merge two sorted link lists of different lengths', () => {
+  const listOne = {
+    val: 1,
+    next: {
+      val: 2,
+      next: {
+        val: 4,
+        next: null
+      }
+    }
+  }
+
+  const listTwo = {
+    val: 1,
+    next: {
+      val: 3,
+      next: null
+    }
+  }
+
+  const expected = {
+    val: 1,
+    next: {
+      val: 1,
+      next: {
+        val: 2,
+        next: {
+          val: 3,
           next: {
             val: 4,
             next: null
@@ -40,8 +81,5 @@ const expected = {
       }
     }
   }
-}
-
-test('should merge two sorted link lists', () => {
   expect(mergeSortedListsIterative(listOne, listTwo)).toEqual(expected)
 })
