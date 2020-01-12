d32b970e38ee72db69b957548ef1c4df32bca92f
andrew-t-james <ajames20@gmail.com>
2020-01-11 18:58:30 -0600

    Update test to cover missed if check

--- challenges/index-of/index.test.js
+++ challenges/index-of/index.test.js
@@ -6,4 +6,5 @@ test('Test Case #1', () => {
   expect(strStr('hello', 'll')).toBe(2)
   expect(strStr('hello', 'lll')).toBe(-1)
   expect(strStr('hello', 'h')).toBe(0)
+  expect(strStr('hello', 'hellohello')).toBe(-1)
 })
