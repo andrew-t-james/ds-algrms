1fe563c7718d28262520ab14636afe8dc3ad1170
andrew-t-james <ajames20@gmail.com>
2020-01-11 18:56:04 -0600

    Add inde-of

---
+++ challenges/index-of/index.js
@@ -0,0 +1,23 @@
+//Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+function strStr(haystack, needle) {
+  if (needle.length === 0) return 0
+  if (needle.length > haystack.length) return -1
+
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === needle[0]) {
+      let sub = haystack.substring(i, i + needle.length)
+      if (sub === needle) {
+        return i
+      }
+    }
+  }
+
+  return -1
+}
+
+module.exports = strStr
---
+++ challenges/index-of/index.test.js
@@ -0,0 +1,9 @@
+const strStr = require('.')
+
+test('Test Case #1', () => {
+  expect(strStr('hello', '')).toBe(0)
+  expect(strStr('hello', 'l')).toBe(2)
+  expect(strStr('hello', 'll')).toBe(2)
+  expect(strStr('hello', 'lll')).toBe(-1)
+  expect(strStr('hello', 'h')).toBe(0)
+})
