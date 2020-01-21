db58472194eb861d57398396a7821881424e2f53
andrew-t-james <ajames20@gmail.com>
2020-01-21 06:44:42 -0600

    Add merge sorted arrays

---
+++ challenges/merge-sorted-arrays/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1, m, nums2, n) {}
+
+module.exports = merge
