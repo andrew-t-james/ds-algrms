7a8fece98c6c016a4b7cc52cd7d0c8f7a070c615
andrew-t-james <ajames20@gmail.com>
2020-01-21 07:16:32 -0600

    Add solution for sortedSquares

--- challenges/squares-sorted-array/index.js
+++ challenges/squares-sorted-array/index.js
@@ -1,5 +1,7 @@
 // Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
 
-function sortedSquares(arr) {}
+function sortedSquares(arr) {
+  return arr.map(n => Math.abs(n) * Math.abs(n)).sort((a, b) => a - b)
+}
 
 module.exports = sortedSquares
--- challenges/squares-sorted-array/index.test.js
+++ challenges/squares-sorted-array/index.test.js
@@ -1,11 +1,6 @@
 const sortedSquares = require('.')
 
 test('should return an array of the squares of each number', () => {
-  expect(sortedSquares(sortedSquares([-4, -1, 0, 3, 10]))).toEqual([
-    0,
-    1,
-    9,
-    16,
-    100
-  ])
+  expect(sortedSquares([-4, -1, 0, 3, 10])).toEqual([0, 1, 9, 16, 100])
+  expect(sortedSquares([-7, -3, 2, 3, 11])).toEqual([4, 9, 9, 49, 121])
 })
