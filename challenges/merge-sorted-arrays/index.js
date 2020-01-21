c61f53ddf36064bf8e0f0aa81fe5f15b12b377f4
andrew-t-james <ajames20@gmail.com>
2020-01-21 07:07:42 -0600

    Add comment block

--- challenges/merge-sorted-arrays/index.js
+++ challenges/merge-sorted-arrays/index.js
@@ -6,6 +6,7 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1, m, nums2, n) {
+  // remove m elements from array and replace with nums1.length
   nums1.splice(m, nums1.length)
   nums1.push(...nums2.splice(0, n))
   nums1.sort((a, b) => a - b)
