e4ee6e5d942fa322b4eabf7f7fd54908789afe53
andrew-t-james <ajames20@gmail.com>
2020-01-25 11:50:55 +0100

    Add findFirstUniqueUsingDictionary

--- challenges/find-first-non-repeat/index.test.js
+++ challenges/find-first-non-repeat/index.test.js
@@ -5,5 +5,5 @@ test('should return the first integer which is unique in the array ', () => {
 })
 
 test('should return the first integer which is unique in the array ', () => {
-  expect(findFirstUnique([9, 2, 3, 2, 6, 6])).toBe(9)
+  expect(findFirstUniqueDictionary([9, 2, 3, 2, 6, 6])).toBe(9)
 })
