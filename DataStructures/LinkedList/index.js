92f9632f474a40526169477e42df1dfcd0222943
andrew-t-james <ajames20@gmail.com>
2020-01-28 11:02:11 +0200

    Add insertAtPosition

--- DataStructures/LinkedList/index.js
+++ DataStructures/LinkedList/index.js
@@ -12,7 +12,47 @@ class LinkedList {
     return this
   }
 
-  isEmpty() {
+  insertAtTail(data) {
+    const node = new Node(data)
+    if (this.isEmpty) {
+      this.head = node
+    } else {
+      let curr = this.head
+      while (curr.next !== null) {
+        curr = curr.next
+      }
+      curr.next = node
+    }
+
+    return this
+  }
+
+  insertAtPosition(data, pos) {
+    const node = new Node(data)
+    if (this.isEmpty) {
+      this.head = node
+    } else {
+      let current = this.getHeadNode
+      let previous
+      let count = 0
+      while (count < pos) {
+        previous = current
+        current = current.next
+        count++
+      }
+
+      node.next = current
+      previous.next = node
+    }
+
+    return this
+  }
+
+  get getHeadNode() {
+    return this.head
+  }
+
+  get isEmpty() {
     return this.head === null
   }
 }
--- DataStructures/LinkedList/index.test.js
+++ DataStructures/LinkedList/index.test.js
@@ -3,13 +3,13 @@ const LinkedList = require('.')
 describe('LinkedList', () => {
   test('should return boolean true when isEmpty', () => {
     const list = new LinkedList()
-    expect(list.isEmpty()).toBe(true)
+    expect(list.isEmpty).toBe(true)
   })
 
   test('should return boolean false when isEmpty', () => {
     const list = new LinkedList()
     list.insertAtHead(1)
-    expect(list.isEmpty()).toBe(false)
+    expect(list.isEmpty).toBe(false)
   })
 
   test('should insert new node at head', () => {
@@ -19,5 +19,28 @@ describe('LinkedList', () => {
     expect(list).toEqual({ head: { data: 1, next: null } })
     list.insertAtHead(2)
     expect(list).toEqual({ head: { data: 2, next: { data: 1, next: null } } })
+  })
+
+  test('should insert new node at tail', () => {
+    const list = new LinkedList()
+    expect(list).toEqual({ head: null })
+    list.insertAtTail(1)
+    expect(list).toEqual({ head: { data: 1, next: null } })
+    list.insertAtTail(2)
+    expect(list).toEqual({ head: { data: 1, next: { data: 2, next: null } } })
+  })
+
+  test('should insert new node at position', () => {
+    const list = new LinkedList()
+    expect(list).toEqual({ head: null })
+    list.insertAtHead(1)
+    expect(list).toEqual({ head: { data: 1, next: null } })
+    list.insertAtTail(2)
+    expect(list).toEqual({ head: { data: 1, next: { data: 2, next: null } } })
+    list.insertAtPosition(3, 1)
+    // console.log('list:', list)
+    expect(list).toEqual({
+      head: { data: 1, next: { data: 3, next: { data: 2, next: null } } }
+    })
   })
 })
