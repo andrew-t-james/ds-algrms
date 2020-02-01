2caaa4c5e954a7b20e1f4582904e05a82d33c0ed
andrew-t-james <ajames20@gmail.com>
2020-02-01 21:52:25 +0200

    Add push and pop

--- DataStructures/Stacks/index.js
+++ DataStructures/Stacks/index.js
@@ -1,12 +1,31 @@
 class Stack {
   constructor() {
     this.items = []
-    this.stackTop = null
+    this.top = null
   }
 
-  get top() {
-    if (this.stackTop == null) return null
-    return this.stackTop
+  push(elem) {
+    this.items.push(elem)
+    this.top = elem
+  }
+
+  pop() {
+    if (this.items.length !== 0) {
+      if (this.items.length === 1) {
+        this.top = null
+        return this.items.pop()
+      } else {
+        this.top = this.items[this.items.length - 2]
+        return this.items.pop()
+      }
+    } else {
+      return null
+    }
+  }
+
+  get getTop() {
+    if (this.top == null) return null
+    return this.top
   }
 
   get isEmpty() {
--- DataStructures/Stacks/index.test.js
+++ DataStructures/Stacks/index.test.js
@@ -1,18 +1,40 @@
 const Stack = require('.')
 
 describe('Stack', () => {
+  function addNodes(stack, nodes) {
+    for (let node of nodes) {
+      stack.push(node)
+    }
+  }
   test('should return boolean when isEmpty called', () => {
     const list = new Stack()
     expect(list.isEmpty).toBe(true)
+    list.push(2)
+    expect(list.isEmpty).toBe(false)
   })
 
   test('should topNode in stack || null if none', () => {
     const list = new Stack()
     expect(list.top).toBe(null)
+    list.push(2)
+    expect(list.top).toBe(2)
   })
 
   test('should return size of stack', () => {
     const list = new Stack()
     expect(list.size).toBe(0)
+    list.push(2)
+    expect(list.size).toBe(1)
+  })
+
+  test('should pop node off Stack and return node', () => {
+    const list = new Stack()
+
+    const nodes = [1, 2, 3, 4, 5]
+    addNodes(list, nodes)
+
+    expect(list.top).toBe(5)
+    expect(list.pop()).toBe(5)
+    expect(list.top).toBe(4)
   })
 })
